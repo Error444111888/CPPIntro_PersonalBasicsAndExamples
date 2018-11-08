eb194575c445e74ab6554488ea201fe2e2be46ac
error418 <error418@hotmail.com>
2018-11-08 01:48:10 -0800

    Initial Commit

---
+++ CPPIntro_PersonalBasicsAndExamples/Integer Function Example.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+
+	/* Integer, function example*/
+	int MyLuckyNumber = 3;
+	cout << MyLuckyNumber;
+
+	cin.get();
+	return 0;
+}
---
+++ CPPIntro_PersonalBasicsAndExamples/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*Basically the Hello World example*/
+int main()
+{
+	cout << "The start of a new era! --- by Ray" << endl;
+	cout << "Second line here... after endl \n \n";
+
+	cout << "The start of a new era! --- by Ray \n";
+	cout << "Second line here starts with backslash n instead of endl";
+	
+	cin.get();
+	return 0;
+
+}
+
+
+int SEcond()
+{
+
+	/* Integer, function example*/
+	int MyLuckyNumber = 3;
+	cout << MyLuckyNumber;
+
+	return 0;
+
+
+
+}
+
